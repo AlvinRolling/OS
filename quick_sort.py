4a77772bee6118f7c5ce1cb4c146f0e81ca437cd
AlvinRolling <595463155@qq.com>
2015-12-16 22:34:20 +0800

    threshold 1000

--- quick_sort.py
+++ quick_sort.py
@@ -31,26 +31,6 @@ def data_split(source):
 	back = source[mid:length]
 	del source[:]	# may not be efficient 
 	return front,back
-'''
-def data_merge(front_num,back_num):
-	sort = []
-	len1 = len(front_num)
-	len2 = len(back_num)
-	if(len1 == 0):
-		return back_num
-	elif(len2 == 0):
-		return front_num
-	if(front_num[0]>back_num[0]):
-		sort.append(back_num[0])
-		return sort+data_merge(front_num[:],back_num[1:])
-	else:
-		sort.append(front_num[0])
-		return sort+data_merge(front_num[1:],back_num[:])
-	# merge two lists using iterations
-'''
-# the data length is too long, can't use the iteration
-
-
 
 def quickSort(L, low, high):
     i = low 
@@ -74,37 +54,19 @@ def quickSort(L, low, high):
 
 def data_merge(front_num,back_num):
 	sort = []
-	# len1 = len(front_num)
-	# len2 = len(back_num)
-	# while(len1 > 0 and len2 > 0):
-	# 	if(front_num[0] > back_num[0]):
-	# 		sort.append(back_num.pop(0))
-	# 		len2 = len2-1
-	# 	else:
-	# 		sort.append(front_num.pop(0))
-	# 		len1 = len1-1
-	# if(len1 == 0):
-	# 	while(len2 > 0):
-	# 		sort.append(back_num.pop(0))
-	# 		len2 = len2-1
-	# elif(len2 == 0):
-	# 	while(len1 > 0):
-	# 		sort.append(front_num.pop(0))
-	# 		len1 = len1-1
-	# del front_num
-	# del back_num
-	# return sort
 	l = front_num+back_num
 	length = len(l)
 	sort = quickSort(l,0,length-1)
 	return sort
 	# attention, smaller number comes first
 
-def distribution(l,queue,pipe):
+def distribution(l,queue,pipe,process_con):
 	length = len(l)
-	if(length < 50000):
+	process_con.acquire()
+	if(length < 1000):
 		sort = quickSort(l,0,length-1)
 		print "length small enough"
+		process_con.release()
 		# not split any more
 	else:
 		queue1 = multiprocessing.Queue()
@@ -113,15 +75,15 @@ def distribution(l,queue,pipe):
 		pipe2 = multiprocessing.Pipe()
 		# use the queue and pipe
 		front,back = data_split(l)
-		pro1 = multiprocessing.Process(target = distribution,args = (front,queue1,pipe1[0]))
-		pro2 = multiprocessing.Process(target = distribution,args = (back,queue2,pipe2[0]))
+		process_con.release()
+		pro1 = multiprocessing.Process(target = distribution,
+			args = (front,queue1,pipe1[0],process_con))
+		pro2 = multiprocessing.Process(target = distribution,
+			args = (back,queue2,pipe2[0],process_con))
 		pro1.start()
 		pro2.start()
 		print pipe1[1].recv()
-
 		print pipe2[1].recv()
-		# import pdb
-		# pdb.set_trace()
 		# start the process, and wait for them to terminate
 		front_num = queue1.get()
 		back_num = queue2.get()
@@ -130,30 +92,27 @@ def distribution(l,queue,pipe):
 		del queue2
 		# delete the queue
 		sort = data_merge(front_num,back_num)
-		# import pdb
-		# pdb.set_trace()
 		# merge two rows of data ranked 
 	queue.put(sort)
 	pipe.send('finished')
-	# print len(sort)
-		# put the sorted data in the queue
 		
 
 if __name__ == '__main__':
 	t1 = time.time()
 	l = read_data()
 	t2 = time.time()
+	print "Read File Time:",t2-t1
 	# read data
 	queue = multiprocessing.Queue()
 	pipe = multiprocessing.Pipe()
-	s = multiprocessing.Process(target = distribution,args = (l,queue,pipe[0]))
+	process_con = multiprocessing.Semaphore(value = 20)
+	s = multiprocessing.Process(target = distribution,args = (l,queue,pipe[0],process_con))
 	s.start()
 	pipe[1].recv()
 	sort = queue.get()
 	t3 = time.time()
+	print "Sort Time:",t3-t2
 	# use the queue to send messages
 	write_data(sort)
 	t4 = time.time()
-	print t2-t1
-	print t3-t2
-	print t4-t3
+	print "Write File Time:",t4-t3
